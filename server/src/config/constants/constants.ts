5b2df6c1afff3033025df0cf56d1811e5814dad7
Dmytro Prysiazhnyi <dimpris@gmail.com>
2019-02-28 05:43:17 +0200

    Changed DB connection

--- server/src/config/constants/constants.ts
+++ server/src/config/constants/constants.ts
@@ -3,7 +3,7 @@
  */
 
 class Constants {
-    static DB_CONNECTION_STRING: string = process.env.NODE_ENV === 'production' ? process.env.dbURI : "mongodb://localhost:27017/quickStart"
+    static DB_CONNECTION_STRING: string = process.env.NODE_ENV === 'production' ? process.env.dbURI : "mongodb://vntumongo:e4Og0O37HyK4D9T@cluster0-shard-00-00-frshe.mongodb.net:27017,cluster0-shard-00-01-frshe.mongodb.net:27017,cluster0-shard-00-02-frshe.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
 }
 Object.seal(Constants);
 export = Constants;
